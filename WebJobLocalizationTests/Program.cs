da078daa483eef3142373ab0963b329fd4836866
subgurim <subgurim@gmail.com>
2015-03-24 19:22:53 +0100

    Inital commit

---
+++ WebJobLocalizationTests/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace WebJobLocalizationTests
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+            Console.WriteLine(Resource.HelloWorld);
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("es-ES");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("es-ES");
+            Console.WriteLine(Resource.HelloWorld);
+
+            Console.ReadLine();
+        }
+    }
+}
